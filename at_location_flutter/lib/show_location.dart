27e737d54693fb108cd3214a25cdb72d64936925
nitesh <niteshk@geekyants.com>
2021-02-24 11:23:15 +0530

    Added zoom restricions, changed bg color of map, removed fnWhenZoomChanges

--- at_location_flutter/lib/show_location.dart
+++ at_location_flutter/lib/show_location.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:latlong/latlong.dart';
-
 import 'common_components/build_marker.dart';
 import 'location_modal/hybrid_model.dart';
 import 'map_content/flutter_map/flutter_map.dart';
-import 'map_content/flutter_map_marker_cluster/src/marker_cluster_layer_options.dart';
 import 'map_content/flutter_map_marker_cluster/src/marker_cluster_plugin.dart';
 import 'utils/constants/constants.dart';
 
@@ -56,40 +54,15 @@ class _ShowLocationState extends State<ShowLocation> {
         ),
         layers: [
           TileLayerOptions(
-            fnWhenZoomChanges: (zoom) => fnWhenZoomChanges(zoom),
             minNativeZoom: 2,
             maxNativeZoom: 18,
-            minZoom: 2,
+            minZoom: 1,
             urlTemplate:
                 "https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=${MixedConstants.MAP_KEY}",
           ),
-          MarkerClusterLayerOptions(
-            maxClusterRadius: 190,
-            disableClusteringAtZoom: 16,
-            size: Size(5, 5),
-            anchor: AnchorPos.align(AnchorAlign.center),
-            fitBoundsOptions: FitBoundsOptions(
-              padding: EdgeInsets.all(50),
-            ),
-            markers: showMarker ? [marker] : [],
-            builder: (context, markers) {},
-          ),
+          MarkerLayerOptions(markers: [marker]),
         ],
       )),
     );
-  }
-
-  fnWhenZoomChanges(double zoom) {
-    if (noPointReceived) return;
-    if ((zoom > 2) && (!showMarker)) {
-      setState(() {
-        showMarker = true;
-      });
-    }
-    if ((zoom < 2) && (showMarker)) {
-      setState(() {
-        showMarker = false;
-      });
-    }
   }
 }
