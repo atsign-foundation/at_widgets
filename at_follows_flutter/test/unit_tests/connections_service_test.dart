679a9540ff75d93862961cc04b977a1cc4c1077d
Sameeraja-Bharathula <sameerajabns@gmail.com>
2021-05-13 13:20:13 +0530

    testcase for wavi and namespace support

--- at_follows_flutter/test/unit_tests/connections_service_test.dart
+++ at_follows_flutter/test/unit_tests/connections_service_test.dart
@@ -55,6 +55,44 @@ void main() {
       expect(
           _connectionsService.following.list.contains(receiverAtsign), isFalse);
     });
+
+    test('to support wavi and persona namespace', () async {
+      var firstAtSign = '@bob🛠';
+      var bobClientService = await setUpFunc(firstAtSign);
+      var metadata = Metadata()
+        ..isPublic = true
+        ..namespaceAware = false;
+      var bobFirstname = AtKey()
+        ..key = 'firstname.wavi'
+        ..metadata = metadata;
+      var bobLastname = AtKey()
+        ..key = 'lastname.wavi'
+        ..metadata = metadata;
+
+      await bobClientService.atClient.put(bobFirstname, 'Bob');
+      await bobClientService.atClient.put(bobLastname, 'Geller');
+
+      var secondAtSign = '@colin🛠';
+      var colinClientService = await setUpFunc(secondAtSign);
+      var metadata1 = Metadata()..isPublic = true;
+      var colinFirstname = AtKey()
+        ..key = 'firstname'
+        ..metadata = metadata1;
+      var colinLastname = AtKey()
+        ..key = 'lastname'
+        ..metadata = metadata1;
+
+      await colinClientService.atClient.put(colinFirstname, 'Colin');
+      await colinClientService.atClient.put(colinLastname, 'Felton');
+
+      Atsign atsign = await _connectionsService.follow(firstAtSign);
+      expect(atsign.subtitle, 'Bob Geller');
+      expect(_connectionsService.following.list.contains(firstAtSign), isTrue);
+
+      Atsign atsign1 = await _connectionsService.follow(secondAtSign);
+      expect(atsign1.subtitle, 'Colin Felton');
+      expect(_connectionsService.following.list.contains(secondAtSign), isTrue);
+    });
   });
 
   group('test unfollow functionality', () {
@@ -106,11 +144,7 @@ Future<AtClientService> setUpFunc(String atsign) async {
   atClient.getSyncManager().init(atsign, preference,
       atClient.getRemoteSecondary(), atClient.getLocalSecondary());
   await atClient.getSyncManager().sync();
-  await atClient
-      .getLocalSecondary()
-      .putValue(AT_PKAM_PRIVATE_KEY, demo_data.pkamPrivateKeyMap[atsign]);
-  await atClient.getLocalSecondary().putValue(
-      AT_ENCRYPTION_PRIVATE_KEY, demo_data.encryptionPrivateKeyMap[atsign]);
+  await setEncryptionKeys(atClient, atsign);
   return atClientService;
 }
 
@@ -133,4 +167,36 @@ AtClientPreference getAtSignPreference(String atsign) {
   preference.privateKey = demo_data.pkamPrivateKeyMap[atsign];
   preference.rootDomain = 'vip.ve.atsign.zone';
   return preference;
+}
+
+setEncryptionKeys(AtClientImpl atClient, String atsign) async {
+  try {
+    var metadata = Metadata();
+    metadata.namespaceAware = false;
+    var result;
+    // set pkam private key
+    result = await atClient.getLocalSecondary().putValue(AT_PKAM_PRIVATE_KEY,
+        demo_data.pkamPrivateKeyMap[atsign]); // set pkam public key
+    result = await atClient
+        .getLocalSecondary()
+        .putValue(AT_PKAM_PUBLIC_KEY, demo_data.pkamPublicKeyMap[atsign]);
+    // set encryption private key
+    result = await atClient.getLocalSecondary().putValue(
+        AT_ENCRYPTION_PRIVATE_KEY, demo_data.encryptionPrivateKeyMap[atsign]);
+    //set aesKey
+    result = await atClient
+        .getLocalSecondary()
+        .putValue(AT_ENCRYPTION_SELF_KEY, demo_data.aesKeyMap[atsign]);
+
+    // set encryption public key. should be synced
+    metadata.isPublic = true;
+    var atKey = AtKey()
+      ..key = 'publickey'
+      ..metadata = metadata;
+    result =
+        await atClient.put(atKey, demo_data.encryptionPublicKeyMap[atsign]);
+    print(result);
+  } catch (e) {
+    print('setting localKeys throws $e');
+  }
 }
