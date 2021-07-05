64b01f270948a70ad37ed7d5320b3467a42cef98
Nitesh Kumar <niteshk@geekyants.com>
2021-07-05 16:34:04 +0530

    Added zoom out button

--- at_location_flutter/lib/event_show_location.dart
+++ at_location_flutter/lib/event_show_location.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:latlong/latlong.dart';
 
+import 'common_components/floating_icon.dart';
 import 'common_components/marker_cluster.dart';
 import 'common_components/popup.dart';
 import 'location_modal/hybrid_model.dart';
@@ -27,47 +28,62 @@ Widget eventShowLocation(List<HybridModel> users, LatLng venue) {
 
   var _eventData = users[users.indexWhere((e) => e.latLng == venue)];
 
-  return FlutterMap(
-    key: UniqueKey(),
-    mapController: mapController,
-    options: MapOptions(
-      center: venue,
-      zoom: markers.isNotEmpty ? 8 : 2,
-      plugins: [MarkerClusterPlugin(UniqueKey())],
-      onTap: (_) => _popupController.hidePopup(),
-    ),
-    layers: [
-      TileLayerOptions(
-        minNativeZoom: 2,
-        maxNativeZoom: 18,
-        minZoom: 2,
-        urlTemplate:
-            'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=${MixedConstants.MAP_KEY}',
-      ),
-      MarkerClusterLayerOptions(
-        maxClusterRadius: 190,
-        disableClusteringAtZoom: 16,
-        size: Size(200, 150),
-        anchor: AnchorPos.align(AnchorAlign.center),
-        fitBoundsOptions: FitBoundsOptions(
-          padding: EdgeInsets.all(50),
+  return Stack(
+    children: [
+      FlutterMap(
+        key: UniqueKey(),
+        mapController: mapController,
+        options: MapOptions(
+          center: venue,
+          zoom: markers.isNotEmpty ? 8 : 2,
+          plugins: [MarkerClusterPlugin(UniqueKey())],
+          onTap: (_) => _popupController.hidePopup(),
         ),
-        markers: markers,
-        polygonOptions: PolygonOptions(
-            borderColor: Colors.blueAccent,
-            color: Colors.black12,
-            borderStrokeWidth: 3),
-        popupOptions: PopupOptions(
-            popupSnap: PopupSnap.top,
-            popupController: _popupController,
-            popupBuilder: (_, marker) {
-              return _popupController.streamController!.isClosed
-                  ? Text('Closed')
-                  : buildPopup(users[markers.indexOf(marker)], center: venue);
+        layers: [
+          TileLayerOptions(
+            minNativeZoom: 2,
+            maxNativeZoom: 18,
+            minZoom: 2,
+            urlTemplate:
+                'https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=${MixedConstants.MAP_KEY}',
+          ),
+          MarkerClusterLayerOptions(
+            maxClusterRadius: 190,
+            disableClusteringAtZoom: 16,
+            size: Size(200, 150),
+            anchor: AnchorPos.align(AnchorAlign.center),
+            fitBoundsOptions: FitBoundsOptions(
+              padding: EdgeInsets.all(50),
+            ),
+            markers: markers,
+            polygonOptions: PolygonOptions(
+                borderColor: Colors.blueAccent,
+                color: Colors.black12,
+                borderStrokeWidth: 3),
+            popupOptions: PopupOptions(
+                popupSnap: PopupSnap.top,
+                popupController: _popupController,
+                popupBuilder: (_, marker) {
+                  return _popupController.streamController!.isClosed
+                      ? Text('Closed')
+                      : buildPopup(users[markers.indexOf(marker)],
+                          center: venue);
+                }),
+            builder: (context, markers) {
+              return buildMarkerCluster(markers, eventData: _eventData);
+            },
+          ),
+        ],
+      ),
+      Positioned(
+        top: 100,
+        right: 0,
+        child: FloatingIcon(
+            icon: Icons.zoom_out_map,
+            onPressed: () {
+              _popupController.hidePopup();
+              mapController.move(venue, 4);
             }),
-        builder: (context, markers) {
-          return buildMarkerCluster(markers, eventData: _eventData);
-        },
       ),
     ],
   );
