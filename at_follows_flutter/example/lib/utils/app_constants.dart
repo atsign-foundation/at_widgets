e045dd587b8234d349205c1cc49985090840eedd
Sitaram <kalluriramkumar>
2021-07-08 18:37:47 +0530

    deleted unnecessary files

--- at_follows_flutter/example/lib/utils/app_constants.dart
+++ at_follows_flutter/example/lib/utils/app_constants.dart
@@ -2,4 +2,5 @@ class AppConstants {
   static String rootDomain = 'root.atsign.wtf';
   static String appNamespace = 'persona';
   static String regex = '.$appNamespace@';
+  static String devAPIKey = '477b-876u-bcez-c42z-6a3d';
 }
