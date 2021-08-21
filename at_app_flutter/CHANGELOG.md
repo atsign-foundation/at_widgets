892244320a514ca5aa7a0563305751c828b681b4
XavierChanth <xchanthavong@gmail.com>
2021-08-21 12:18:34 -0400

    Add at_app_flutter repo

---
+++ at_app_flutter/CHANGELOG.md
@@ -0,0 +1,3 @@
+## 0.1.0
+
+- Initialize with library portion from at_app package.
---
+++ at_app_flutter/lib/src/models/at_env.dart
@@ -0,0 +1,29 @@
+import 'package:dotenv/dotenv.dart' as dot;
+
+/// AtEnv is a helper class to load in the environment variables from the .env file
+class AtEnv {
+  /// Load the environment variables from the .env file.
+  /// Directly calls load from the dotenv package.
+  static load() => dot.load();
+
+  /// Returns the root domain from the environment.
+  /// Root domain is used to control what root server you want to use for the app.
+  static final rootDomain = _getEnvVar('ROOT_DOMAIN') ?? 'root.atsign.org';
+
+  /// Returns the namespace from the environment.
+  /// Namespace is used to filter by an app's namespace from the secondary server.
+  static final appNamespace = _getEnvVar('NAMESPACE') ?? 'at_skeleton_app';
+
+  /// Returns the app api key from the environment.
+  /// The api key used to generate free @signs by at_onboarding_flutter.
+  /// Also used to pay commissions to developers (email )
+  static final appApiKey = _getEnvVar('API_KEY') ?? '';
+
+  /// Returns the value of environment variable [key] loaded from the environment.
+  /// Can be used for other environment variables beyond what is included by at_app.
+  static String? _getEnvVar(String key) {
+    var value = dot.env[key];
+    if (value?.isEmpty ?? false) return null;
+    return value;
+  }
+}
