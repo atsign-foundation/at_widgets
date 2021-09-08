64d120398a55370ab1d47d31e0b2d3ad63fbc158
yahu1031 <hi2minnu@gmail.com>
2021-09-08 14:40:31 +0530

    updated creds

--- at_onboarding_flutter/example/lib/utils/app_constants.dart
+++ at_onboarding_flutter/example/lib/utils/app_constants.dart
@@ -1,9 +1,6 @@
 class AppConstants {
-  static String rootDomain = 'root.atsign.org';
+  static String rootDomain = 'root.atsign.wtf';
   static String appNamespace = 'follows';
   static String regex = '.$appNamespace@';
-  // Uncomment the following line if you want to test as developer.
-  static String devAPIKey = '400b-806u-bzez-z42z-6a3p';
-  // Comment the following line if you want to test as developer.
-  // static String devAPIKey = '477b-876u-bcez-c42z-6a3d';
+  static String devAPIKey = '477b-876u-bcez-c42z-6a3d';
 }
