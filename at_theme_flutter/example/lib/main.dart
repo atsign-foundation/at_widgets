a899dcb4fc9d78cd7d1eea4f11c58c13d804ff26
Sachin Singh <sachins@geekyants.com>
2021-09-17 00:43:48 +0530

    theme save under progress

--- at_theme_flutter/example/lib/main.dart
+++ at_theme_flutter/example/lib/main.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
+import 'package:at_onboarding_flutter/screens/onboarding_widget.dart';
 import 'package:at_theme_flutter/at_theme_flutter.dart';
+import 'package:example/client_sdk_service.dart';
+import 'package:example/src/utils/constants.dart';
 import 'package:flutter/material.dart';
-
+import 'package:at_client_mobile/at_client_mobile.dart';
 import 'src/pages/profile_page.dart';
 
 void main() {
@@ -12,8 +15,20 @@ void main() {
 final StreamController<AppTheme> appThemeController =
     StreamController<AppTheme>.broadcast();
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ClientSdkService clientSdkService = ClientSdkService.getInstance();
+
+  @override
+  void initState() {
+    clientSdkService.onboard();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<AppTheme>(
@@ -26,7 +41,91 @@ class MyApp extends StatelessWidget {
           child: MaterialApp(
             title: 'Multi theme',
             theme: appTheme.toThemeData(),
-            home: ProfilePage(),
+            home: Scaffold(
+              appBar: AppBar(
+                title: const Text('Plugin example app'),
+              ),
+              body: Builder(
+                builder: (context) => Column(
+                  children: [
+                    Container(
+                        padding: EdgeInsets.all(10.0),
+                        child: Center(
+                          child: Text(
+                              'A client service should create an atClient instance and call onboard method before navigating to QR scanner screen',
+                              textAlign: TextAlign.center),
+                        )),
+                    Center(
+                        child: TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.black12),
+                            ),
+                            onPressed: () async {
+                              Onboarding(
+                                context: context,
+                                atClientPreference:
+                                    clientSdkService.atClientPreference,
+                                domain: MixedConstants.ROOT_DOMAIN,
+                                appAPIKey: MixedConstants.devAPIKey,
+                                appColor: Color.fromARGB(255, 240, 94, 62),
+                                onboard: (Map<String?, AtClientService> value,
+                                    String? atsign) async {
+                                  clientSdkService.atClientServiceInstance =
+                                      value[atsign];
+                                  clientSdkService.setCurrentAtsign = atsign;
+                                  print(
+                                      'clientSdkService.atClientServiceInstance 1 : ${clientSdkService.atClientServiceInstance}');
+                                  await Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => ProfilePage()));
+                                },
+                                onError: (error) async {
+                                  await showDialog(
+                                      context: context,
+                                      builder: (BuildContext context) {
+                                        return AlertDialog(
+                                          content: Text('Something went wrong'),
+                                          actions: [
+                                            TextButton(
+                                                onPressed: () {
+                                                  Navigator.of(context).pop();
+                                                },
+                                                child: Text('ok'))
+                                          ],
+                                        );
+                                      });
+                                },
+                              );
+                            },
+                            child: Text(
+                              'Show QR scanner screen',
+                              style: TextStyle(color: Colors.black),
+                            ))),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    Center(
+                        child: TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.black12),
+                            ),
+                            onPressed: () async {
+                              await Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => ProfilePage()));
+                            },
+                            child: Text(
+                              'Already authenticated',
+                              style: TextStyle(color: Colors.black),
+                            ))),
+                  ],
+                ),
+              ),
+            ),
           ),
         );
       },
