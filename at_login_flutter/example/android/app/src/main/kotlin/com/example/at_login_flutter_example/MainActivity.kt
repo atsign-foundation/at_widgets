4972d3b18f0f55f993b5b7ce1c53d5c46af9a9dd
nickelskevin <nickelskevin@gmail.com>
2021-09-16 21:38:27 -0700

    refactoring example to use at_app

--- at_login_flutter/example/android/app/src/main/kotlin/com/example/at_login_flutter_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.at_login_flutter_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- at_login_flutter/example/lib/utils/app_constants.dart
+++
@@ -1,6 +0,0 @@
-class AppConstants {
-  static String appTitle = 'AtLogin Demo App';
-  static String rootDomain = 'root.atsign.org';
-  static String appNamespace = 'at_login';
-  static String regex = '.$appNamespace@';
-}
