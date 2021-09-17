946b45793f3f2b53b9737a574cd2339e435911e6
yahu1031 <hi2minnu@gmail.com>
2021-09-17 17:36:41 +0530

    updated docs

--- at_onboarding_flutter/example/lib/main.dart
+++ at_onboarding_flutter/example/lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:at_client/src/preference/at_client_preference.dart';
-import 'package:at_client_mobile/src/at_client_service.dart';
-import 'package:at_onboarding_flutter/utils/app_constants.dart';
+import 'package:at_client_mobile/at_client_mobile.dart';
 import 'package:at_onboarding_flutter_example/dashboard.dart';
 import 'package:at_onboarding_flutter_example/services/at_service.dart';
+import 'package:at_onboarding_flutter_example/utils/app_constants.dart';
 import 'package:at_onboarding_flutter_example/utils/app_strings.dart';
 import 'package:flutter/material.dart';
 import 'package:at_onboarding_flutter/widgets/custom_reset_button.dart';
@@ -11,17 +10,19 @@ import 'package:at_onboarding_flutter/at_onboarding_flutter.dart';
 import 'package:at_utils/at_logger.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   late AtClientPreference atClientPrefernce;
-  AtSignLogger _logger = AtSignLogger('Plugin example app');
+  final AtSignLogger _logger = AtSignLogger('Plugin example app');
   @override
   void initState() {
     AtService.getInstance().getAtClientPreference().then((AtClientPreference value) => atClientPrefernce = value);
@@ -43,6 +44,8 @@ class _MyAppState extends State<MyApp> {
                     onPressed: () async {
                       Onboarding(
                         context: context,
+                        // This domain parameter is optional.
+                        domain: AppConstants.rootDomain,
                         atClientPreference: atClientPrefernce,
                         appColor: const Color.fromARGB(255, 240, 94, 62),
                         onboard: (Map<String?, AtClientService> value, String? atsign) {
@@ -53,7 +56,9 @@ class _MyAppState extends State<MyApp> {
                           _logger.severe('Onboarding throws $error error');
                         },
                         rootEnvironment: RootEnvironment.Staging,
-                        nextScreen: DashBoard(),
+                        // API Key is mandatory for production environment.
+                        // appAPIKey: YOUR_API_KEY_HERE
+                        nextScreen: const DashBoard(),
                       );
                     },
                     child: const Text(AppStrings.scan_qr)),
