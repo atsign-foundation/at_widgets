d330827088e040e6bf7226cce72402299ff11ec9
XavierChanth <xchanthavong@gmail.com>
2021-09-30 09:44:37 -0400

    Add types for at_env

--- at_app_flutter/lib/src/models/at_env.dart
+++ at_app_flutter/lib/src/models/at_env.dart
@@ -8,16 +8,16 @@ class AtEnv {
 
   /// Returns the root domain from the environment.
   /// Root domain is used to control what root server you want to use for the app.
-  static final rootDomain = _getEnvVar('ROOT_DOMAIN') ?? 'root.atsign.org';
+  static final String rootDomain = _getEnvVar('ROOT_DOMAIN') ?? 'root.atsign.org';
 
   /// Returns the namespace from the environment.
   /// Namespace is used to filter by an app's namespace from the secondary server.
-  static final appNamespace = _getEnvVar('NAMESPACE') ?? 'at_skeleton_app';
+  static final String appNamespace = _getEnvVar('NAMESPACE') ?? 'at_skeleton_app';
 
   /// Returns the app api key from the environment.
   /// The api key used to generate free @signs by at_onboarding_flutter.
   /// Also used to pay commissions to developers (email )
-  static final appApiKey = _getEnvVar('API_KEY') ?? '';
+  static final String? appApiKey = _getEnvVar('API_KEY');
 
   /// Returns the value of environment variable [key] loaded from the environment.
   /// Can be used for other environment variables beyond what is included by at_app.
