2c9105ff1465cfee5961c58279eb1c09b04aed94
XavierChanth <xchanthavong@gmail.com>
2021-11-17 11:45:32 -0500

    Remove redunant line of code

--- at_app_flutter/CHANGELOG.md
+++ at_app_flutter/CHANGELOG.md
@@ -1,4 +1,4 @@
-## 4.0.0-rc+1
+## 4.0.0-rc+2
 
 - Adding desktop support by replacing flutter_config with flutter_dotenv
 
--- at_app_flutter/lib/src/models/at_env.dart
+++ at_app_flutter/lib/src/models/at_env.dart
@@ -1,16 +1,12 @@
 import 'package:at_onboarding_flutter/at_onboarding_flutter.dart'
     show RootEnvironment;
-import 'package:flutter/material.dart' show WidgetsFlutterBinding;
 import 'package:flutter_dotenv/flutter_dotenv.dart' show dotenv;
 
 /// AtEnv is a helper class to load in the environment variables from the .env file
 class AtEnv {
   /// Load the environment variables from the .env file.
   /// Directly calls load from the dotenv package.
-  static Future<void> load() async {
-    WidgetsFlutterBinding.ensureInitialized();
-    await dotenv.load();
-  }
+  static Future<void> load() => dotenv.load();
 
   /// Returns the root domain from the environment.
   /// Root domain is used to control what root server you want to use for the app.
