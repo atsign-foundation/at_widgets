33b597e3f2e56fe755db682858ac3badf7e552e1
Gayathri-GA <gayathrid@geekyants.com>
2021-12-28 14:05:22 +0530

    Completed creating documentation template for at_notify_flutter

--- at_notify_flutter/at_notify_flutter_example/lib/second_screen.dart
+++ at_notify_flutter/at_notify_flutter_example/lib/second_screen.dart
@@ -1,31 +1,173 @@
 import 'package:flutter/material.dart';
 import 'package:at_client_mobile/at_client_mobile.dart';
+import 'package:at_notify_flutter/screens/notify_screen.dart';
+import 'package:at_notify_flutter/services/notify_service.dart';
+import 'package:at_notify_flutter/utils/init_notify_service.dart';
+import 'package:at_notify_flutter/utils/notify_utils.dart';
+
+import 'constants.dart';
+import 'package:at_lookup/at_lookup.dart';
 
 //* The next screen after onboarding (second screen)
-class SecondScreen extends StatelessWidget {
+class SecondScreen extends StatefulWidget {
   const SecondScreen({Key? key}) : super(key: key);
 
   @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  String? activeAtSign;
+  var atClientManager = AtClientManager.getInstance();
+  late AtClientPreference atClientPreference;
+  TextEditingController atSignController = TextEditingController(text: '');
+  TextEditingController messageController = TextEditingController(text: '');
+
+  @override
+  void initState() {
+    getAtSignAndInitializeNotify();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    /// Get the AtClientManager instance
-    var atClientManager = AtClientManager.getInstance();
-
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
-        title: const Text('Dashboard'),
+        title: const Text('Notify Example'),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const Text(
-                'Successfully onboarded and navigated to FirstAppScreen'),
-
-            /// Use the AtClientManager instance to get the current atsign
-            Text(
-                'Current @sign: ${atClientManager.atClient.getCurrentAtSign()}'),
-          ],
+      body: Builder(
+        builder: (context) => Center(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Welcome $activeAtSign',
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextField(
+                  controller: atSignController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: '@atSign'),
+                  onChanged: (text) {},
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                TextField(
+                  controller: messageController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(), hintText: 'Enter Message'),
+                  onChanged: (text) {},
+                ),
+                SizedBox(
+                  height: 16.0,
+                ),
+                TextButton(
+                  onPressed: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              NotifyScreen(notifyService: NotifyService())),
+                    );
+                  },
+                  child: Text(
+                    'Get past notifications',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                TextButton(
+                  onPressed: _sendMessage,
+                  child: Text(
+                    'Notify Text',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  _sendMessage() async {
+    if (atSignController.text.isEmpty) {
+      print('atSignController isEmpty');
+      showSnackBar('Enter atsign');
+      return;
+    }
+
+    if (messageController.text.isEmpty) {
+      print('message isEmpty');
+      showSnackBar('Enter message');
+      return;
+    }
+
+    var _isValidAtsign = await checkAtsign();
+    if (!_isValidAtsign) {
+      showSnackBar('Atsign not valid');
+      return;
+    }
+
+    var _res = await notifyText(
+      context,
+      activeAtSign,
+      atSignController.text,
+      messageController.text,
+    );
+    if (_res) {
+      messageController.clear();
+      showSnackBar('Message sent succesfully', color: Colors.green);
+    } else {
+      showSnackBar('Something went wrong');
+    }
+  }
+
+  Future<bool> checkAtsign() async {
+    if (atSignController.text.isEmpty) {
+      return false;
+    } else if (!atSignController.text.contains('@')) {
+      atSignController.text = '@' + atSignController.text;
+    }
+    var checkPresence = await AtLookupImpl.findSecondary(
+        atSignController.text, MixedConstants.ROOT_DOMAIN, 64);
+    return checkPresence != null;
+  }
+
+  void getAtSignAndInitializeNotify() async {
+    var currentAtSign = await atClientManager.atClient.getCurrentAtSign();
+    setState(() {
+      activeAtSign = currentAtSign;
+    });
+    initializeNotifyService(
+      atClientManager,
+      activeAtSign!,
+      atClientPreference,
+      rootDomain: MixedConstants.ROOT_DOMAIN,
+    );
+  }
+
+  showSnackBar(String text, {Color color = Colors.red}) {
+    ScaffoldMessenger.of(scaffoldKey.currentContext!).showSnackBar(SnackBar(
+      backgroundColor: color,
+      dismissDirection: DismissDirection.horizontal,
+      content: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 14,
+          letterSpacing: 0.1,
+          fontWeight: FontWeight.normal,
+        ),
+      ),
+    ));
   }
 }
