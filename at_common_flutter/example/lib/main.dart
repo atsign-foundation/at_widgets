e51cd5575253881ee071b996815f2ae066cbd387
sonle <sonle@geekyants.com>
2022-01-06 21:35:25 +0700

    feat: at_common_flutter switch theme

--- at_common_flutter/example/lib/main.dart
+++ at_common_flutter/example/lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:async';
+
+import 'package:at_common_flutter/utils/text_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:at_common_flutter/at_common_flutter.dart';
+
+final StreamController<ThemeMode> updateThemeMode = StreamController<ThemeMode>.broadcast();
 
 void main() {
   runApp(MyApp());
@@ -9,25 +14,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Example App',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Example App Home Page'),
+    return StreamBuilder<ThemeMode>(
+      stream: updateThemeMode.stream,
+      initialData: ThemeMode.light,
+      builder: (context, snapshot) {
+        return MaterialApp(
+          theme: ThemeData(
+            brightness: Brightness.light,
+            primaryColor: Color(0xFF6200EE),
+            accentColor: Color(0xFF03DAC6),
+            backgroundColor: Colors.white,
+          ),
+          darkTheme: ThemeData(
+            brightness: Brightness.dark,
+            fontFamily: 'RobotoSlab',
+            primaryColor: Color(0xFF3700B3),
+            accentColor: Color(0xFF018786),
+            backgroundColor: Colors.black,
+          ),
+          themeMode: snapshot.data,
+          title: 'Example App',
+          home: MyHomePage(title: 'Example App Home Page'),
+        );
+      },
     );
   }
 }
@@ -62,19 +71,40 @@ class _MyHomePageState extends State<MyHomePage> {
     SizeConfig().init(context);
     return Scaffold(
       appBar: CustomAppBar(
+        appBarColor: Theme.of(context).primaryColor,
         showBackButton: false,
         showTitle: true,
         titleText: widget.title,
+        titleTextStyle: CustomTextStyles.primaryBold18.copyWith(
+          color: Theme.of(context).brightness == Brightness.light
+              ? Colors.black
+              : Colors.white,
+        ),
+        showLeadingIcon: true,
+        leadingIcon: Icon(
+          Icons.home_outlined,
+          color: Theme.of(context).brightness == Brightness.light
+              ? Colors.black
+              : Colors.white,
+        ),
         onTrailingIconPressed: () {
           print('Trailing icon of appbar pressed');
         },
         showTrailingIcon: true,
         trailingIcon: Center(
-          child: Icon(
-            Icons.add,
-            color: Theme.of(context).brightness == Brightness.light
-                ? Colors.black
-                : Colors.white,
+          child: IconButton(
+            onPressed: () {
+              updateThemeMode.sink.add(
+                  Theme.of(context).brightness == Brightness.light
+                      ? ThemeMode.dark
+                      : ThemeMode.light);
+            },
+            icon: Icon(
+              Theme.of(context).brightness == Brightness.light
+                  ? Icons.dark_mode_outlined
+                  : Icons.light_mode_outlined,
+              color: Theme.of(context).accentColor,
+            ),
           ),
         ),
       ),
@@ -113,6 +143,9 @@ class _MyHomePageState extends State<MyHomePage> {
               value: (String val) {
                 print('Current value of input field: $val');
               },
+              inputFieldColor: Theme.of(context).brightness == Brightness.light
+                  ? Colors.black.withOpacity(0.2)
+                  : Colors.white.withOpacity(0.2),
             ),
             Divider(
               color: Color(0xFFBEC0C8),
