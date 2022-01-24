6a7b2fa217ba317f971a225169dba1d72e6da24c
Gayathri-GA <gayathrid@geekyants.com>
2022-01-24 12:57:33 +0530

    Pub.dev Documentation updated for at_chat_flutter package

--- at_chat_flutter/README.md
+++ at_chat_flutter/README.md
@@ -1,28 +1,59 @@
-<img src="https://atsign.dev/assets/img/@developersmall.png?sanitize=true">
+<img src="https://atsign.dev/assets/img/@platform_logo_grey.svg?sanitize=true">
 
-### Now for some internet optimism.
+## Now for some internet optimism.
+
+[![pub package](https://img.shields.io/pub/v/at_chat_flutter)](https://pub.dev/packages/at_chat_flutter) [![pub points](https://badges.bar/at_chat_flutter/pub%20points)](https://pub.dev/packages/at_chat_flutter/score) [![build status](https://github.com/atsign-foundation/at_client_sdk/actions/workflows/at_client_sdk.yaml/badge.svg?branch=trunk)](https://github.com/atsign-foundation/at_client_sdk/actions/workflows/at_client_sdk.yaml) [![gitHub license](https://img.shields.io/badge/license-BSD3-blue.svg)](./LICENSE)
+
 
 # at_chat_flutter
 
+## Introduction
+
 A flutter plugin to provide chat feature between two atsigns.
-
-## Getting Started
-
 This plugin provides a chat screen - ChatScreen that can be accessed as a bottom sheet or as a navigated screen.
 
-### Initialising
+## Get Started:
+
+Initially to get a basic overview of the SDK, you must read the [atsign docs](https://atsign.dev/docs/overview/).
+
+> To use this package you must be having a basic setup, Follow here to [get started](https://atsign.dev/docs/get-started/setup-your-env/).
+
+
+### Installation:
+
+ To use this library in your app, add it to your pubspec.yaml
+
+``` 
+  dependencies:
+    at_chat_flutter: ^3.0.3
+```
+#### Add to your project
+
+ ```dart
+ flutter pub get 
+ ```
+ #### Import in your application code
+
+ ```dart
+ import 'package:at_chat_flutter/at_chat_flutter.dart';
+ ```
+### Clone it from github
+
+ Feel free to fork a copy of the source from the [GitHub Repo](https://github.com/atsign-foundation/at_widgets)
+
+### Initialising:
 The chat service needs to be initialised. It is expected that the app will first create an AtClientService instance using the preferences and then use it to initialise the chat service.
 
-```
+```dart
 initializeChatService(
         clientSdkService.atClientServiceInstance!.atClientManager,
         activeAtSign!,
         rootDomain: MixedConstants.ROOT_DOMAIN);
 ```
 
-### Sample Usage
+### Usage
 
-As a bottom sheet
+### As a bottom sheet
 ```
 FlatButton(
     onPressed: () {
@@ -36,7 +67,7 @@ FlatButton(
 ),
 ```
 
-As a screen
+### As a screen
 ```
 class ThirdScreen extends StatefulWidget {
   @override
@@ -58,3 +89,12 @@ class _ThirdScreenState extends State<ThirdScreen> {
   }
 }
 ```
+
+## Example
+
+We have a good example with explanation in the [at_chat_flutter](https://pub.dev/packages/at_chat_flutter/example) package.
+
+## Open source usage and contributions
+
+ This is freely licensed open source code, so feel free to use it as is, suggest changes or enhancements or create your
+ own version. See [CONTRIBUTING.md](https://github.com/atsign-foundation/at_widgets/blob/trunk/CONTRIBUTING.md) for detailed guidance on how to setup tools, tests and make a pull request.
---
+++ at_chat_flutter/at_chat_flutter_example/lib/second_screen.dart
@@ -0,0 +1,269 @@
+import 'package:at_chat_flutter/at_chat_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:at_client_mobile/at_client_mobile.dart';
+
+import 'constants.dart';
+import 'third_screen.dart';
+
+class SecondScreen extends StatefulWidget {
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  AtClientService? atClientService;
+  String? activeAtSign;
+  GlobalKey<ScaffoldState>? scaffoldKey;
+  String chatWithAtSign = '';
+  bool showOptions = false;
+
+  // for goup chat
+  String groupId = '';
+  String member1 = '';
+  String member2 = '';
+
+  /// Get the AtClientManager instance
+  var atClientManager = AtClientManager.getInstance();
+  @override
+  void initState() {
+    getAtSignAndInitializeChat();
+    scaffoldKey = GlobalKey<ScaffoldState>();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(title: Text('Second Screen')),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+              child: Text(
+                'Welcome $activeAtSign!',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text('Enter an atsign to chat with'),
+            SizedBox(
+              height: 10.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextFormField(
+                autofocus: true,
+                onChanged: (value) {
+                  chatWithAtSign = value;
+                  if (showOptions) {
+                    setState(() {
+                      showOptions = false;
+                    });
+                  }
+                },
+                // validator: Validators.validateAdduser,
+                decoration: InputDecoration(
+                  prefixText: '@',
+                  prefixStyle: TextStyle(color: Colors.grey),
+                  hintText: '\tEnter user atsign',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            showOptions
+                ? Column(
+                    children: [
+                      SizedBox(height: 20.0),
+                      TextButton(
+                        onPressed: () {
+                          scaffoldKey!.currentState!
+                              .showBottomSheet((context) => ChatScreen());
+                        },
+                        child: Container(
+                          height: 40,
+                          child: Text(
+                            'Open chat in bottom sheet',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ThirdScreen()));
+                        },
+                        child: Container(
+                          height: 40,
+                          child: Text(
+                            'Navigate to chat screen',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          if (chatWithAtSign != null &&
+                              chatWithAtSign.trim() != '') {
+                            setAtsignToChatWith();
+                            setState(() {
+                              showOptions = true;
+                            });
+                          } else {
+                            showAtsignErrorDialog(context);
+                          }
+                        },
+                        child: Container(
+                          height: 40,
+                          child: Text(
+                            'Chat options',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+            Divider(
+              thickness: 2,
+              height: 20,
+            ),
+            Text('Group chat:'),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextFormField(
+                autofocus: false,
+                onChanged: (value) {
+                  groupId = value;
+                },
+                // validator: Validators.validateAdduser,
+                decoration: InputDecoration(
+                  hintText: '\tEnter the group ID',
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextFormField(
+                autofocus: false,
+                onChanged: (value) {
+                  member1 = value;
+                },
+                // validator: Validators.validateAdduser,
+                decoration: InputDecoration(
+                  prefixText: '@',
+                  prefixStyle: TextStyle(color: Colors.grey),
+                  hintText: '\tEnter first user atsign',
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: TextFormField(
+                autofocus: false,
+                onChanged: (value) {
+                  member2 = value;
+                },
+                // validator: Validators.validateAdduser,
+                decoration: InputDecoration(
+                  prefixText: '@',
+                  prefixStyle: TextStyle(color: Colors.grey),
+                  hintText: '\tEnter second user atsign',
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+              ),
+              onPressed: () {
+                setGroupToChatWith(context);
+              },
+              child: Text(
+                'Show group chat',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void getAtSignAndInitializeChat() async {
+    var currentAtSign = atClientManager.atClient.getCurrentAtSign();
+    setState(() {
+      activeAtSign = currentAtSign;
+    });
+    initializeChatService(atClientManager, activeAtSign!,
+        rootDomain: MixedConstants.ROOT_DOMAIN);
+  }
+
+  void setAtsignToChatWith() {
+    setChatWithAtSign(chatWithAtSign);
+  }
+
+  void setGroupToChatWith(BuildContext context) {
+    print('setGroupToChatWith');
+    if (member1 != null &&
+        member1.trim() != '' &&
+        member2 != null &&
+        member2.trim() != '' &&
+        groupId != null &&
+        groupId != '') {
+      setChatWithAtSign(null, isGroup: true, groupId: groupId, groupMembers: [
+        activeAtSign!,
+        member1.startsWith('@') ? member1 : '@' + member1,
+        member2.startsWith('@') ? member2 : '@' + member2
+      ]);
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => ThirdScreen()));
+    } else {
+      showAtsignErrorDialog(context);
+    }
+  }
+
+  void showAtsignErrorDialog(BuildContext context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Row(
+              children: [Text('Some details are missing!')],
+            ),
+            content: Text('Please enter all fields'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Close',
+                  style: TextStyle(color: Colors.black),
+                ),
+              )
+            ],
+          );
+        });
+  }
+}
