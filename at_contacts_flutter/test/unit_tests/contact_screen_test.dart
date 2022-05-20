ec538717f6ea05a1814bc9bd36e6137f4dd449bd
Gayathri-GA <gayathrid@geekyants.com>
2022-05-20 13:07:09 +0530

    Unit tests for at_contacts_flutter added

--- at_contacts_flutter/test/unit_tests/contact_screen_test.dart
+++ at_contacts_flutter/test/unit_tests/contact_screen_test.dart
@@ -1,12 +1,19 @@
+import 'package:at_contacts_flutter/services/contact_service.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-//class MockContactService extends Mock implements  {}
+class MockContactService extends Mock implements ContactService {}
 
 void main() {
-  // group('Contact Screen test', () {
-  // chat service is initialized
-  // test('Contact service is initialized', () async {
-  // });
-  // });
+  ContactService mockContactService = MockContactService();
+  group('Contact Screen test', () {
+    setUp(() {
+      reset(mockContactService);
+    });
+    test('Contacts fetched form contacts service', () {
+      when(() => mockContactService.fetchContacts().then((_) async {
+            print('Contacts fetched successfully');
+          }));
+    });
+  });
 }
