02048f404aa40075764c33f749bde9cc9cc6ffbc
Sachin Singh <sachins@geekyants.com>
2022-06-21 16:54:24 +0530

    fix #640: images removed before saving data in key- buffer size limit

--- at_contacts_group_flutter/lib/services/group_service.dart
+++ at_contacts_group_flutter/lib/services/group_service.dart
@@ -148,6 +148,8 @@ class GroupService {
 
   /// Function to create a group
   Future<dynamic> createGroup(AtGroup atGroup) async {
+    atGroup = removeImageFromAtGroupMembers(atGroup);
+
     try {
       var group = await atContactImpl.createGroup(atGroup);
       if (group is AtGroup) {
@@ -241,6 +243,12 @@ class GroupService {
   /// Function to add members to a group
   Future<dynamic> addGroupMembers(
       List<AtContact?> contacts, AtGroup group) async {
+    for (var i = 0; i < contacts.length; i++) {
+      if (contacts[i]!.tags != null && contacts[i]!.tags!['image'] != null) {
+        contacts[i]!.tags!['image'] = null;
+      }
+    }
+
     try {
       var result = await atContactImpl.addMembers(Set.from(contacts), group);
       await updateGroupStreams(group, expandGroup: true);
@@ -253,6 +261,7 @@ class GroupService {
 
   /// Function to update group details
   Future<dynamic> updateGroup(AtGroup group, {int? expandIndex}) async {
+    group = removeImageFromAtGroupMembers(group);
     try {
       var updatedGroup = await atContactImpl.updateGroup(group);
       if (updatedGroup is AtGroup) {
@@ -290,6 +299,7 @@ class GroupService {
 
   Future<dynamic> updateGroupData(AtGroup group, BuildContext context,
       {bool isDesktop = false, int? expandIndex}) async {
+    group = removeImageFromAtGroupMembers(group);
     try {
       var result = await updateGroup(group, expandIndex: expandIndex);
       if (isDesktop) {
@@ -457,6 +467,17 @@ class GroupService {
     } catch (e) {
       selectedContactsSink.add([]);
     }
+  }
+
+  AtGroup removeImageFromAtGroupMembers(AtGroup atGroup) {
+    for (var i = 0; i < atGroup.members!.length; i++) {
+      if (atGroup.members!.elementAt(i).tags != null &&
+          atGroup.members!.elementAt(i).tags!['image'] != null) {
+        atGroup.members!.elementAt(i).tags!['image'] = null;
+      }
+    }
+
+    return atGroup;
   }
 
   /// Function to reset all data related to groups
--- at_contacts_group_flutter/lib/widgets/desktop_image_picker.dart
+++ at_contacts_group_flutter/lib/widgets/desktop_image_picker.dart
@@ -2,8 +2,10 @@ import 'dart:typed_data';
 // ignore: import_of_legacy_library_into_null_safe
 import 'package:at_utils/at_logger.dart';
 import 'package:file_selector/file_selector.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:image_compression/image_compression.dart';
 
-dynamic desktopImagePicker() async {
+Future<Uint8List?> desktopImagePicker() async {
   AtSignLogger atSignLogger = AtSignLogger('desktopImagePicker');
   try {
     // ignore: omit_local_variable_types
@@ -17,8 +19,20 @@ dynamic desktopImagePicker() async {
     }
     // ignore: omit_local_variable_types
     final XFile file = files[0];
-    final Uint8List image = await file.readAsBytes();
-    return image;
+
+    final input = ImageFile(
+      rawBytes: await file.readAsBytes(),
+      filePath: file.path,
+    );
+
+    final output = compress(ImageFileConfiguration(
+        input: input,
+        config: const Configuration(
+            jpgQuality: 50,
+            pngCompression: PngCompression.defaultCompression,
+            outputType: OutputType.jpg)));
+
+    return output.rawBytes;
   } catch (e) {
     atSignLogger.severe('Error in desktopImagePicker $e');
     return null;
