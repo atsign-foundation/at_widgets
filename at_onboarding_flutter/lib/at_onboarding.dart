f2279284bf08599ca336b85b2265c7fcba86177f
sonle <sonle@geekyants.com>
2022-06-29 16:06:32 +0700

    update onboarding function

--- at_onboarding_flutter/lib/at_onboarding.dart
+++ at_onboarding_flutter/lib/at_onboarding.dart
@@ -10,44 +10,51 @@ import 'services/at_onboarding_config.dart';
 import 'screen/at_onboarding_start_screen.dart';
 
 class AtOnboarding {
+  /// Using this function to get onboard atsing.
+  ///
+  /// @param context The build context.
+  /// @param config The config for the onboard
+  /// @param isSwitchingAtsign True - alway show UI for add new atsign. False - check onboard if existing atsing. Default is false
+  /// @param atsign The atsign name when change the primary atsign.
+  ///
+  /// Return [AtOnboardingResult]
   static Future<AtOnboardingResult> onboard({
     required BuildContext context,
     required AtOnboardingConfig config,
+    bool isSwitchingAtsign = false,
+    String? atsign,
   }) async {
     AtOnboardingConstants.setApiKey(config.appAPIKey ??
         (AtOnboardingConstants.rootEnvironment.apikey ?? ''));
     AtOnboardingConstants.rootDomain =
         config.domain ?? AtOnboardingConstants.rootEnvironment.domain;
-    final result = await showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (_) => AtOnboardingStartScreen(config: config),
-    );
-    if (result is AtOnboardingResult) {
-      return result;
-    } else {
-      return AtOnboardingResult.cancelled();
-    }
-  }
 
-  static Future<AtOnboardingResult> start({
-    required BuildContext context,
-    required AtOnboardingConfig config,
-    VoidCallback? onSuccess,
-    VoidCallback? onError,
-  }) async {
-    AtOnboardingConstants.setApiKey(config.appAPIKey ??
-        (AtOnboardingConstants.rootEnvironment.apikey ?? ''));
-    AtOnboardingConstants.rootDomain =
-        config.domain ?? AtOnboardingConstants.rootEnvironment.domain;
-    final result = await Navigator.push(context,
-        MaterialPageRoute(builder: (BuildContext context) {
-      return AtOnboardingHomeScreen(config: config);
-    }));
-    if (result is AtOnboardingResult) {
-      return result;
+    if (!isSwitchingAtsign || (atsign ?? '').trim().isNotEmpty) {
+      if ((atsign ?? '').trim().isNotEmpty) {
+        await changePrimaryAtsign(atsign: atsign!);
+      }
+      //Check if existing an atsign => return onboard success
+      final result = await showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (_) => AtOnboardingStartScreen(config: config),
+      );
+      if (result is AtOnboardingResult) {
+        return result;
+      } else {
+        return AtOnboardingResult.cancelled();
+      }
     } else {
-      return AtOnboardingResult.cancelled();
+      //Navigate user to screen to add new atsign
+      final result = await Navigator.push(context,
+          MaterialPageRoute(builder: (BuildContext context) {
+        return AtOnboardingHomeScreen(config: config);
+      }));
+      if (result is AtOnboardingResult) {
+        return result;
+      } else {
+        return AtOnboardingResult.cancelled();
+      }
     }
   }
 
