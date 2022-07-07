9a50ffcdd59ab8318c644e97d644f0410c6a8b53
Minnu <hi2minnu@gmail.com>
2022-07-07 14:37:20 +0530

    Added atSyncUIOverlay to none by default

--- at_sync_ui_flutter/lib/services/at_sync_ui_services.dart
+++ at_sync_ui_flutter/lib/services/at_sync_ui_services.dart
@@ -17,7 +17,7 @@ class AtSyncUIService {
   Function? onSuccessCallback, onErrorCallback;
   var syncService;
   AtSyncUIStyle atSyncUIStyle = AtSyncUIStyle.cupertino;
-  AtSyncUIOverlay atSyncUIOverlay = AtSyncUIOverlay.dialog;
+  AtSyncUIOverlay atSyncUIOverlay = AtSyncUIOverlay.none;
   bool showTextWhileSyncing = true;
 
   final StreamController _atSyncUIListenerController =
@@ -78,12 +78,8 @@ class AtSyncUIService {
 
   /// calls sync and shows selected UI
   /// [atSyncUIOverlay] decides whether dialog or snackbar to be shown while syncing
-  void sync({AtSyncUIOverlay? atSyncUIOverlay}) {
-    assert(syncService != null, "AtSyncUIService not initialised");
-
-    if (atSyncUIOverlay != null) {
+  void sync({AtSyncUIOverlay atSyncUIOverlay = AtSyncUIOverlay.none}) {
       this.atSyncUIOverlay = atSyncUIOverlay;
-    }
 
     /// change status to syncing
     _atSyncUIListenerSink.add(AtSyncUIStatus.syncing);
