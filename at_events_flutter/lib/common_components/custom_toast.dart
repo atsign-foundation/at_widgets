cf850ac7332430b5a40c4b3a957030ae304ba99d
Minnu <hi2minnu@gmail.com>
2022-08-23 12:06:30 +0530

    fix: made context non-nullable

--- at_events_flutter/lib/common_components/custom_toast.dart
+++ at_events_flutter/lib/common_components/custom_toast.dart
@@ -9,7 +9,7 @@ class CustomToast {
   factory CustomToast() => _instance;
 
   /// pass [isError] true to have a red bg, [isSuccess] true to have a green bg.
-  void show(String message, BuildContext? context,
+  void show(String message, BuildContext context,
           {Color? bgColor,
           Color? textColor,
           double? toastWidth,
@@ -18,7 +18,7 @@ class CustomToast {
           double gravity = 10.0,
           bool isError = false,
           bool isSuccess = false}) =>
-      ScaffoldMessenger.of(context!).showSnackBar(
+      ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text(
             message,
