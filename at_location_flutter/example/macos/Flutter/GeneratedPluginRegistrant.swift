1722ade5f65eb78b82ad7abae59bd5df40147252
Sardar1208 <sarthak.bakre@gmail.com>
2022-08-26 14:05:47 +0530

    added testcases for myLocation.dart

--- at_location_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ at_location_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,20 +5,22 @@
 import FlutterMacOS
 import Foundation
 
+import at_file_saver
 import biometric_storage
-import file_saver
 import file_selector_macos
 import geolocator_apple
+import package_info_plus_macos
 import path_provider_macos
 import share_plus_macos
 import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  BiometricStorageMacOSPlugin.register(with: registry.registrar(forPlugin: "BiometricStorageMacOSPlugin"))
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
+  BiometricStorageMacOSPlugin.register(with: registry.registrar(forPlugin: "BiometricStorageMacOSPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
--- at_location_flutter/test/at_location_flutter_test.dart
+++ at_location_flutter/test/at_location_flutter_test.dart
@@ -1,5 +1,58 @@
+import 'package:at_location_flutter/service/my_location.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+Position get mockPosition => Position(
+    latitude: 52.561270,
+    longitude: 5.639382,
+    timestamp: DateTime.fromMillisecondsSinceEpoch(
+      500,
+      isUtc: true,
+    ),
+    altitude: 3000.0,
+    accuracy: 0.0,
+    heading: 0.0,
+    speed: 0.0,
+    speedAccuracy: 0.0);
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
+  setUp(() {
+    GeolocatorPlatform.instance = MockGeolocatorPlatform();
+  });
+
+  test("get_my_location", () async {
+    var res = await getMyLocation();
+    expect(res, isA<LatLng>());
+  });
+
+  test("is_location_enabled", () async {
+    var res = await isLocationServiceEnabled();
+    expect(res, true);
+  });
+
+  test("get_current_position", () async {
+    var res = await getCurrentPosition();
+    expect(res, isA<LatLng>());
+  });
+}
+
+class MockGeolocatorPlatform extends Mock
+    with MockPlatformInterfaceMixin
+    implements GeolocatorPlatform {
+  @override
+  Future<LocationPermission> checkPermission() =>
+      Future.value(LocationPermission.whileInUse);
+
+  @override
+  Future<bool> isLocationServiceEnabled() => Future.value(true);
+
+  @override
+  Future<Position> getCurrentPosition({
+    LocationSettings? locationSettings,
+  }) =>
+      Future.value(mockPosition);
 }
