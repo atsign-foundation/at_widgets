ba2beb003328912ffe35afeb4c4597258f5e46d8
Sachin Singh <sachins@geekyants.com>
2022-10-20 17:51:19 +0530

    fix: added check if atsign is already activated

--- packages/at_onboarding_flutter/lib/screen/at_onboarding_activate_screen.dart
+++ packages/at_onboarding_flutter/lib/screen/at_onboarding_activate_screen.dart
@@ -14,6 +14,7 @@ import 'package:at_onboarding_flutter/utils/at_onboarding_error_util.dart';
 import 'package:at_onboarding_flutter/utils/at_onboarding_response_status.dart';
 import 'package:at_onboarding_flutter/utils/at_onboarding_strings.dart';
 import 'package:at_onboarding_flutter/widgets/at_onboarding_dialog.dart';
+import 'package:at_server_status/at_server_status.dart';
 import 'package:at_sync_ui_flutter/at_sync_material.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -109,6 +110,15 @@ class _AtOnboardingActivateScreenState
     if (atsign != null) {
       atsign = atsign.split('@').last;
     }
+
+    // check if atSign already activated
+    AtSignStatus? atsignStatus =
+        await OnboardingService.getInstance().checkAtsignStatus(atsign: atsign);
+    if (atsignStatus == AtSignStatus.activated) {
+      await showErrorDialog('atSign already activated.');
+      return;
+    }
+
     dynamic data;
 
     dynamic response = await _freeAtsignService
