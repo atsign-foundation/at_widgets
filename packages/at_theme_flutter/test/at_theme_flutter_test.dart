d48d7752309b9ead5d9ca14cad8aad2d52d82c55
Xavier Lin <xavier.li.1332@gmail.com>
2023-05-16 14:29:44 -0400

    chore(at_theme_flutter): cleaning up dart analyze warnings

--- packages/at_theme_flutter/test/at_theme_flutter_test.dart
+++ packages/at_theme_flutter/test/at_theme_flutter_test.dart
@@ -4,48 +4,57 @@ import 'package:at_theme_flutter/services/theme_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:at_client/src/client/request_options.dart';
 
 class MockAtClientManager extends Mock implements AtClientManager {}
 
 class MockAtClient extends Mock implements AtClient {
   @override
-  Future<bool> put(AtKey key, dynamic value, {bool isDedicated = false}) async{
+  Future<bool> put(AtKey key, dynamic value, {bool isDedicated = false, PutRequestOptions? putRequestOptions}) async {
     return true;
   }
 
   @override
-  Future<AtValue> get(AtKey key, {
+  Future<AtValue> get(
+    AtKey key, {
     bool isDedicated = false,
     GetRequestOptions? getRequestOptions,
-  }) async{
+  }) async {
     return AtValue()
-      ..value = AppTheme(brightness: Brightness.light, primaryColor: const Color(0xfff44336), secondaryColor: Colors.orange, backgroundColor: Colors.white,).encoded();
+      ..value = AppTheme(
+        brightness: Brightness.light,
+        primaryColor: const Color(0xfff44336),
+        secondaryColor: Colors.orange,
+        backgroundColor: Colors.white,
+      ).encoded();
   }
 }
 
 void main() {
   MockAtClientManager mockAtClientManager = MockAtClientManager();
 
-  group('Theme data tests', ()  {
-
+  group('Theme data tests', () {
     var mockAtClient = MockAtClient();
 
-    when(() => mockAtClientManager.atClient)
-        .thenAnswer((_) => mockAtClient);
+    when(() => mockAtClientManager.atClient).thenAnswer((_) => mockAtClient);
     ThemeService().atClientManager = mockAtClientManager;
 
-    test('Update theme data', ()  async {
-      var updateThemeDataResult = await  ThemeService().updateThemeData(AppTheme(brightness: Brightness.light, primaryColor: Colors.red, secondaryColor: Colors.orange, backgroundColor: Colors.white,),);
+    test('Update theme data', () async {
+      var updateThemeDataResult = await ThemeService().updateThemeData(
+        AppTheme(
+          brightness: Brightness.light,
+          primaryColor: Colors.red,
+          secondaryColor: Colors.orange,
+          backgroundColor: Colors.white,
+        ),
+      );
       expect(updateThemeDataResult, true);
     });
 
-    test('Get theme data', ()  async {
+    test('Get theme data', () async {
       var updateThemeDataResult = await ThemeService().getThemeData();
       expect(updateThemeDataResult.runtimeType, AppTheme);
 
       expect(updateThemeDataResult!.primaryColor, const Color(0xfff44336));
     });
-
   });
 }
