edd23556787e2edc9d20f4024734ff3829e718b6
Curtly Critchlow <curtlycritchlow@ccwc.io>
2023-07-03 12:08:24 -0400

    chore: doc comment added to LocationModel class

--- packages/at_location_flutter/lib/location_modal/location_modal.dart
+++ packages/at_location_flutter/lib/location_modal/location_modal.dart
@@ -1,3 +1,12 @@
+/// A class which is used to store the location data.
+///
+/// [lat] is the latitude of the location.
+/// [long] is the longitude of the location.
+/// [displayName] is the name of the location.
+/// [state] is the state of the location.
+/// [city] is the city of the location.
+/// [postalCode] is the postal code of the location.
+
 class LocationModal {
   String? lat, long;
   String? displayName, state, city, postalCode;
@@ -18,7 +27,6 @@ class LocationModal {
     displayName = '''${(displayName ?? '')},
 ${ad['vicinity'].toString().replaceAll('<br/>', ' ')}
     ''';
-    city = (ad['vicinity'] ?? '')
-        .replaceAll('<br/>', ' '); // [vicinity] gives entire address
+    city = (ad['vicinity'] ?? '').replaceAll('<br/>', ' '); // [vicinity] gives entire address
   }
 }
