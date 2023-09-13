8f6b6652c2eabaa8f13ecd0e18ad1fa569003e22
xavierchanth <xchanthavong@gmail.com>
2023-09-13 17:18:18 +0800

    deps(at_invitation_flutter): use new onboarding widget

--- packages/at_invitation_flutter/example/lib/main.dart
+++ packages/at_invitation_flutter/example/lib/main.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:at_invitation_flutter_example/second_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:at_client_mobile/at_client_mobile.dart';
-import 'package:at_onboarding_flutter/at_onboarding_flutter.dart'
-    show Onboarding;
+import 'package:at_onboarding_flutter/at_onboarding_flutter.dart';
 import 'package:at_utils/at_logger.dart' show AtSignLogger;
 import 'package:path_provider/path_provider.dart'
     show getApplicationSupportDirectory;
@@ -67,42 +66,40 @@ class _MyAppState extends State<MyApp> {
                 Center(
                   child: ElevatedButton(
                     onPressed: () async {
-                      var preference = await futurePreference;
-                      setState(() {
-                        atClientPreference = preference;
-                      });
-                      Onboarding(
-                        context: context,
-                        atClientPreference: atClientPreference,
-                        domain: AtEnv.rootDomain,
-                        rootEnvironment: AtEnv.rootEnvironment,
-                        appAPIKey: '477b-876u-bcez-c42z-6a3d',
-                        onboard: (Map<String?, AtClientService> value,
-                            String? atsign) async {
-                          atClientService = value[atsign];
-                          await Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const SecondScreen()));
-                        },
-                        onError: (error) async {
-                          _logger.severe('Onboarding throws $error error');
-                          await showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  content: const Text('Something went wrong'),
-                                  actions: [
-                                    TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: const Text('ok'))
-                                  ],
-                                );
-                              });
-                        },
-                      );
+                      atClientPreference = await futurePreference;
+                      if (context.mounted) {
+                        final result = await AtOnboarding.onboard(
+                          context: context,
+                          config: AtOnboardingConfig(
+                            atClientPreference: atClientPreference,
+                            domain: AtEnv.rootDomain,
+                            rootEnvironment: AtEnv.rootEnvironment,
+                            appAPIKey: AtEnv.appApiKey,
+                          ),
+                        );
+                        switch (result.status) {
+                          case AtOnboardingResultStatus.success:
+                            if (context.mounted) {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) => const SecondScreen()));
+                            }
+                            break;
+                          case AtOnboardingResultStatus.error:
+                            if (context.mounted) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  backgroundColor: Colors.red,
+                                  content: Text('An error has occurred'),
+                                ),
+                              );
+                            }
+                            break;
+                          case AtOnboardingResultStatus.cancel:
+                            break;
+                        }
+                      }
                     },
                     child: const Text('Start onboarding'),
                   ),
