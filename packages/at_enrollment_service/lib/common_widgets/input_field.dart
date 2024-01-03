fb03f7536a38d6eea6e522a5b3cc0e6009144f53
Sardar1208 <sarthak.bakre@gmail.com>
2024-01-03 12:38:57 +0530

    feat: updated onboarding UI

--- packages/at_enrollment_service/lib/common_widgets/input_field.dart
+++ packages/at_enrollment_service/lib/common_widgets/input_field.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class InputField extends StatelessWidget {
   Widget? prefix;
   Widget? suffix;
   String hintText;
+  bool isNumpad;
+  int? maxLength;
   Function(String) onChange;
 
   InputField(
@@ -11,6 +14,8 @@ class InputField extends StatelessWidget {
       this.prefix,
       this.suffix,
       required this.onChange,
+      this.maxLength,
+      this.isNumpad = false,
       this.hintText = ''});
 
   @override
@@ -24,18 +29,28 @@ class InputField extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
+          const SizedBox(width: 5),
           prefix ?? const SizedBox.shrink(),
           const SizedBox(width: 10),
           Expanded(
             child: TextField(
-              decoration:
-                  InputDecoration(border: InputBorder.none, hintText: hintText),
+              maxLength: maxLength,
+              maxLengthEnforcement: MaxLengthEnforcement.enforced,
+              maxLines: 1,
+              keyboardType: isNumpad ? TextInputType.number : TextInputType.text,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: hintText,
+                  counterText: "",
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
               onChanged: (String val) {
                 onChange(val);
               },
             ),
           ),
           suffix ?? const SizedBox.shrink(),
+          const SizedBox(width: 5),
         ],
       ),
     );
