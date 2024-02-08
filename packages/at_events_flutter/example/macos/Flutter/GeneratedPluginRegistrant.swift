2128213421a807b08628a1f75bc536d0299b7ecd
Sardar1208 <sarthak.bakre@gmail.com>
2024-02-08 11:31:27 +0530

    feat: uptake at_commons: 4.0.1 in at_contacts_flutter and at_chat_flutter

--- packages/at_events_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/at_events_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import biometric_storage
 import device_info_plus
 import emoji_picker_flutter
 import file_selector_macos
+import flutter_image_compress_macos
 import geolocator_apple
 import package_info_plus
 import path_provider_foundation
@@ -24,8 +25,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
