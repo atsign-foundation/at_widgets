46a7745b326c7ce0beac9b5d11406f0373a7d169
Curtly Critchlow <curtlycritchlow@ccwc.io>
2024-06-12 09:38:46 -0400

    fix: Invalid use of a private type in a public API info message fixed for at_notify_flutter

--- packages/at_notify_flutter/example/lib/main.dart
+++ packages/at_notify_flutter/example/lib/main.dart
@@ -1,13 +1,12 @@
 import 'dart:async';
 
-import 'package:at_notify_flutter_example/second_screen.dart';
-import 'package:flutter/material.dart';
+import 'package:at_app_flutter/at_app_flutter.dart' show AtEnv;
 import 'package:at_client_mobile/at_client_mobile.dart';
+import 'package:at_notify_flutter_example/second_screen.dart';
 import 'package:at_onboarding_flutter/at_onboarding_flutter.dart'
     show AtOnboarding, AtOnboardingConfig, AtOnboardingResultStatus;
-import 'package:path_provider/path_provider.dart'
-    show getApplicationSupportDirectory;
-import 'package:at_app_flutter/at_app_flutter.dart' show AtEnv;
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart' show getApplicationSupportDirectory;
 
 Future<void> main() async {
   await AtEnv.load();
@@ -24,14 +23,13 @@ Future<AtClientPreference> loadAtClientPreference() async {
     ..isLocalStoreRequired = true;
 }
 
-final StreamController<ThemeMode> updateThemeMode =
-    StreamController<ThemeMode>.broadcast();
+final StreamController<ThemeMode> updateThemeMode = StreamController<ThemeMode>.broadcast();
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
@@ -72,9 +70,7 @@ class _MyAppState extends State<MyApp> {
               actions: <Widget>[
                 IconButton(
                   onPressed: () {
-                    updateThemeMode.sink.add(themeMode == ThemeMode.light
-                        ? ThemeMode.dark
-                        : ThemeMode.light);
+                    updateThemeMode.sink.add(themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light);
                   },
                   icon: Icon(
                     Theme.of(context).brightness == Brightness.light
@@ -106,10 +102,7 @@ class _MyAppState extends State<MyApp> {
                         );
                         switch (result.status) {
                           case AtOnboardingResultStatus.success:
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (_) => const SecondScreen()));
+                            Navigator.push(context, MaterialPageRoute(builder: (_) => const SecondScreen()));
                             break;
                           case AtOnboardingResultStatus.error:
                             ScaffoldMessenger.of(context).showSnackBar(
