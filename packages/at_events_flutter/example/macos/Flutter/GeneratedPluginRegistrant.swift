fe1214c0d6f4496ae7adf31021e64d6e81ab19a3
Curtly Critchlow <curtlycritchlow@ccwc.io>
2024-08-20 11:35:06 -0400

    chore: at_chat_flutter updated to 3.0.13

--- packages/at_events_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/at_events_flutter/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,6 +18,7 @@ import share_plus
 import shared_preferences_foundation
 import sqflite
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
@@ -33,4 +34,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
