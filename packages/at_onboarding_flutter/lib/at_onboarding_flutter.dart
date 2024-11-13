0c032ed28579fcd20649179375ddfb55e33450a9
xavierchanth <xchanthavong@gmail.com>
2024-11-13 08:41:59 -0500

    chore: mv internals to lib/src

--- packages/at_onboarding_flutter/lib/at_onboarding_flutter.dart
+++ packages/at_onboarding_flutter/lib/at_onboarding_flutter.dart
@@ -1,10 +1,15 @@
 library at_onboarding_flutter;
 
-export './utils/at_onboarding_app_constants.dart' show RootEnvironment;
+// Re-export some important external dependencies
 export 'package:at_backupkey_flutter/at_backupkey_flutter.dart';
-export './at_onboarding.dart';
-export 'services/at_onboarding_config.dart';
-export 'at_onboarding_result.dart';
 export 'package:at_client_mobile/at_client_mobile.dart';
+
+// Core package requirements
+export 'src/at_onboarding.dart';
+export 'src/at_onboarding_result.dart';
+export 'src/utils/at_onboarding_app_constants.dart' show RootEnvironment;
+export 'src/services/at_onboarding_config.dart';
+
+// Additional customizations
 export './localizations/generated/l10n.dart';
-export './services/at_onboarding_theme.dart';
+export 'src/services/at_onboarding_theme.dart';
