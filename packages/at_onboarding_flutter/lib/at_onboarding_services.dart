ca5f9576d935835661c93c9a7e5fb1bcba516f97
xavierchanth <xchanthavong@gmail.com>
2024-11-19 22:14:49 -0500

    fix: reduce chances of export collisions

--- packages/at_onboarding_flutter/lib/at_onboarding_services.dart
+++ packages/at_onboarding_flutter/lib/at_onboarding_services.dart
@@ -2,10 +2,14 @@ library at_onboarding_services;
 
 export 'src/services/at_keys_file_upload_service.dart';
 export 'src/services/at_onboarding_backup_service.dart';
-export 'src/services/at_onboarding_config.dart';
 export 'src/services/at_onboarding_theme.dart';
 export 'src/services/at_onboarding_tutorial_service.dart';
 export 'src/services/backend_service.dart';
 export 'src/services/free_atsign_service.dart';
 export 'src/services/onboarding_service.dart';
 export 'src/services/sdk_service.dart';
+
+// Don't export this here even though it's a service,
+// otherwise it will conflict with the at_onboarding_flutter.dart library
+// Please leave this comment here to prevent future pain
+// export 'src/services/at_onboarding_config.dart';
